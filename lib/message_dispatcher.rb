2166fbd1a921488fa901d86f896475df5c18a7b0
tinusagustin <agustinus.andreas@bukalapak.com>
2019-12-22 14:50:00 +0700

    try

--- lib/message_dispatcher.rb
+++ lib/message_dispatcher.rb
@@ -15,4 +15,18 @@ class MessageDispatcher
       @bot.api.sendMessage(chat_id: @chat_id, text: message)
     end
   end
+
+  def send(text: nil, photo: nil, gif: nil)
+    if text
+      @bot.api.send_message(chat_id: @chat_id, text: text)
+    end
+
+    if photo
+      @bot.api.send_photo(chat_id: @chat_id, photo: Faraday::UploadIO.new(photo, 'image/jpeg'))
+    end
+
+    if gif
+      @bot.api.send_document(chat_id: @chat_id, document: Faraday::UploadIO.new(gif, 'image/gif'))
+    end
+  end
 end
