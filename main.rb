6fb9764e355c54c401b49122f6bbcc768e658f37
tinusagustin <agustinus.andreas@bukalapak.com>
2019-12-22 13:49:22 +0700

    try

--- main.rb
+++ main.rb
@@ -11,20 +11,20 @@ loop do
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         case message.text
-        if '/makan2'
+        when '/makan2'
           question = 'Mau makan-makan kapan?'
-          # See more: https://core.telegram.org/bots/api#replykeyboardmarkup
           answers =
             Telegram::Bot::Types::ReplyKeyboardMarkup
             .new(keyboard: [%w(A.sekarang B.besok), %w(C.lusa D.nggakbisasemuanya)], one_time_keyboard: true)
           bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
-        elsif '/tidakmakan2'
+          if message&.text && message&.chat&.id
+            mdp = MessageDispatcher.new(bot: bot, chat_id: message.chat.id)
+            mdp.dispatch CommandWatcher.parse(message.text)
+          end
+        when '/tidakmakan2'
           # See more: https://core.telegram.org/bots/api#replykeyboardremove
           kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
           bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
-        else message&.text && message&.chat&.id
-          mdp = MessageDispatcher.new(bot: bot, chat_id: message.chat.id)
-          mdp.dispatch CommandWatcher.parse(message.text)
         end
       end
     end
